ea2b76cfd3adcd31d2d6da9149e25191e3f1a634
Bilal Arbi <bilalarbi297@gmail.com>
2023-02-07 23:35:13 +0000

    Initial Entry

---
+++ App.js
@@ -0,0 +1,17 @@
+import { StatusBar } from 'expo-status-bar';
+
+import {Text, View} from 'react-native';
+
+const MyMobileApp = () => {
+  return (
+    <View style={{
+      flex: 1,
+justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+      <Text>Hello World!</Text>
+    </View>
+  );
+};
+
+export default MyMobileApp;
